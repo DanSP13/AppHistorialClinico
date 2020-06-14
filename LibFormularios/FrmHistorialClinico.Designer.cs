865328def0556684e4d43c768b731ce7757fb84e
LeandroKotaro <160895@unsaac.edu.pe>
2020-06-14 16:29:57 -0500

    Haciendo formulario historial clinico
    hice esta ves el formaulario historial clinico

--- LibFormularios/FrmHistorialClinico.Designer.cs
+++ LibFormularios/FrmHistorialClinico.Designer.cs
@@ -28,12 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodHistorialClinico = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmedico = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPaciente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpHistorialClinico = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtReceta = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMotivoDeConsulta = new System.Windows.Forms.TextBox();
+            this.dgvHistorialClinico = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialClinico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HISTORIAL CLÍNICO";
+            // 
+            // txtCodHistorialClinico
+            // 
+            this.txtCodHistorialClinico.Location = new System.Drawing.Point(436, 99);
+            this.txtCodHistorialClinico.Name = "txtCodHistorialClinico";
+            this.txtCodHistorialClinico.Size = new System.Drawing.Size(138, 20);
+            this.txtCodHistorialClinico.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtmedico);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPaciente);
+            this.groupBox1.Location = new System.Drawing.Point(52, 125);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(522, 60);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(230, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MÉDICO : ";
+            // 
+            // txtmedico
+            // 
+            this.txtmedico.Location = new System.Drawing.Point(294, 19);
+            this.txtmedico.Name = "txtmedico";
+            this.txtmedico.Size = new System.Drawing.Size(100, 20);
+            this.txtmedico.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PACIENTE : ";
+            // 
+            // txtPaciente
+            // 
+            this.txtPaciente.Location = new System.Drawing.Point(108, 19);
+            this.txtPaciente.Name = "txtPaciente";
+            this.txtPaciente.Size = new System.Drawing.Size(100, 20);
+            this.txtPaciente.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CÓDIGO HISTORIAL CLÍNICO: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(379, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "FECHA : ";
+            // 
+            // dtpHistorialClinico
+            // 
+            this.dtpHistorialClinico.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpHistorialClinico.Location = new System.Drawing.Point(436, 73);
+            this.dtpHistorialClinico.Name = "dtpHistorialClinico";
+            this.dtpHistorialClinico.Size = new System.Drawing.Size(138, 20);
+            this.dtpHistorialClinico.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtReceta);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtMotivoDeConsulta);
+            this.groupBox2.Location = new System.Drawing.Point(52, 191);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(522, 123);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "RECETA : ";
+            // 
+            // txtReceta
+            // 
+            this.txtReceta.Location = new System.Drawing.Point(36, 85);
+            this.txtReceta.Name = "txtReceta";
+            this.txtReceta.Size = new System.Drawing.Size(456, 20);
+            this.txtReceta.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "MOTIVO DE CONSULTA : ";
+            // 
+            // txtMotivoDeConsulta
+            // 
+            this.txtMotivoDeConsulta.Location = new System.Drawing.Point(36, 38);
+            this.txtMotivoDeConsulta.Name = "txtMotivoDeConsulta";
+            this.txtMotivoDeConsulta.Size = new System.Drawing.Size(456, 20);
+            this.txtMotivoDeConsulta.TabIndex = 4;
+            // 
+            // dgvHistorialClinico
+            // 
+            this.dgvHistorialClinico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistorialClinico.Location = new System.Drawing.Point(52, 349);
+            this.dgvHistorialClinico.Name = "dgvHistorialClinico";
+            this.dgvHistorialClinico.Size = new System.Drawing.Size(522, 150);
+            this.dgvHistorialClinico.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(52, 320);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(285, 320);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 11;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(499, 320);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // FrmHistorialClinico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(632, 524);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvHistorialClinico);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dtpHistorialClinico);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtCodHistorialClinico);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmHistorialClinico";
             this.Text = "FrmHistorialClinico";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialClinico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodHistorialClinico;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmedico;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPaciente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpHistorialClinico;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtReceta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMotivoDeConsulta;
+        private System.Windows.Forms.DataGridView dgvHistorialClinico;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
